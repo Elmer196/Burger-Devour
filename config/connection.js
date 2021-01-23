6249c1ebca954f1d232133fa52408afd7b33dd8e
Elmer196 <elmerkobe88@gmail.com>
2021-01-23 13:16:31 -0600

    Changes to the server

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,25 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306,
-    database: "burgers_db",
-    password: "",
-    user: "root",
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost", 
+        port: 3306,
+        database: "burgers_db",
+        password: "",
+        user: "root",
+
+    })
+};
 
 connection.connect(function(err) {
     if(err){
         throw err;
     }
     console.log("Connected!");
-  });
+});
 
-  module.exports = connection;
+module.exports = connection;
